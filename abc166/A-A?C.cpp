95d8e33b7707de25338768e62ca14dbc43eb4522
Aryan Gautam <aryangautam.7778@rediffmail.com>
2020-05-04 18:43:18 +0530

    Create A-A?C.cpp

---
+++ abc166/A-A?C.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main(){
+  string str;
+  cin>>str;
+  if(str=="ARC")
+    cout<<"ABC"<<endl;
+  else
+    cout<<"ARC"<<endl;
+  
+  return 0;
+}
