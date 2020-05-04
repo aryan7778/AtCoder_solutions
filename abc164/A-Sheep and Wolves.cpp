337528aa6992e3668bb487be27c38d09309f0ca8
Aryan Gautam <aryangautam.7778@rediffmail.com>
2020-05-04 18:36:17 +0530

    Create A-Sheep and Wolves.cpp

---
+++ abc164/A-Sheep and Wolves.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<algorithm>
+
+using namespace std;
+int main(){
+	int S,W;
+	cin>>S>>W;
+	if(W>=S)
+	cout<<"unsafe"<<endl;
+	else
+	cout<<"safe"<<endl;
+	return 0;
+}
